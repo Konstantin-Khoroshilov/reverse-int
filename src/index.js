f6ee4fac47a1adf128384ce9c0bae96c92137e10
Konstantin Khoroshilov <manly41142009@yandex.ru>
2024-10-25 17:39:10 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  const string = String(Math.abs(n));
+  const reversedString = string.split('').reverse().join('');
+  return Number(reversedString);
 }
